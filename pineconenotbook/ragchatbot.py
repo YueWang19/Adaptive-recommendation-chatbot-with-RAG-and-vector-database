b1d3fc835f23397a01397dd20e8098812bbd9e48
Yue Wang <wang.yue19@northeastern.edu>
2024-07-16 23:54:26 -0400

    Adjust temperature to 0.2 cause did better performance base on user query

--- pineconenotbook/ragchatbot.py
+++ pineconenotbook/ragchatbot.py
@@ -530,7 +530,7 @@ index = pc.Index(index_name)
 llm = ChatOpenAI(
     openai_api_key=os.environ.get('OPENAI_API_KEY'),
     model_name='gpt-3.5-turbo',
-    temperature=0.0
+    temperature=0.2
 )
 qa = RetrievalQA.from_chain_type(
     llm=llm,
